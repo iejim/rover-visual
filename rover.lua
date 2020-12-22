3e37d675b244889f30142ca844661d8473a632fc
iejim <ij.dakar@gmail.com>
2020-12-21 23:00:16 -0400

    Primer Archivo: Descripción.

---
+++ rover.lua
@@ -0,0 +1,10 @@
+--[[
+  Control de un Rover de Hands-On para entrenamiento 
+  en técnicas básicas de visión usando CoppeliaSim
+
+  Ivan Jiménez, y otros.
+  2021 - Ingeniería Mecatrónica
+  Instituto Tecnológico de Santo Domingo
+
+--]]
+
