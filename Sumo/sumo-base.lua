886b7c40c192292812f1c5152c131299fea5cca2
iejim <ij.dakar@gmail.com>
2020-12-23 16:49:34 -0400

    Inicio del Sumo.

---
+++ Sumo/sumo-base.lua
@@ -0,0 +1,9 @@
+--[[
+Ejémplo básico de un Sumo de Hands-On para
+entrenamiento de técnicas básicas de programación
+para un robot que reacciona a su entorno.
+
+Ivan Jiménez, y otros.
+2021 - Ingeniería Mecatrónica
+Instituto Tecnológico de Santo Domingo
+--]]
